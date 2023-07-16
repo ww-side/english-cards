327a85df0a2a4bb034437e64a495e8118385203d
ww_side <daniil.metelia@gmail.com>
2023-07-16 19:02:40 +0300

    added Home page

---
+++ src/components/pages/Home/data.ts
@@ -0,0 +1,21 @@
+import { nanoid as id } from '@reduxjs/toolkit';
+
+export const data = [
+  {
+    id: id(),
+    title: `With EngCards you can easily create personalised cards to help you
+    remember new words, phrases and grammatical constructions.`,
+  },
+  {
+    id: id(),
+    title: `Creating cards with EngCards is easy and intuitive. You can add 
+    English words and phrases along with their translations, 
+    definitions or usage examples.`,
+  },
+  {
+    id: id(),
+    title: `EngCards also allows you to take tests to check your progress 
+    and reinforce what you have learned. With EngCards, learning English 
+    becomes a convenient and fun process.`,
+  },
+];
---
+++ src/components/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import { FC } from 'react';
+import Footer from '../../common/Footer';
+import { data } from './data.ts';
+import mainImg from '../../../assets/img/main.png';
+
+const Home: FC = () => {
+  return (
+    <div className="bg-gray-100">
+      <div className="flex items-center justify-center gap-7 mt-7 mb-10 max-sm:flex-col">
+        <img
+          className="h-[500px] w-[400px] max-md:h-[400px] max-md:w-[300px]"
+          src={mainImg}
+          alt="iphone"
+        />
+        <div className="w-5/12 max-sm:w-full max-sm:px-6">
+          <h1 className="text-3xl font-semibold mb-2 max-sm:text-center">
+            English Cards
+          </h1>
+          {data.map(item => (
+            <p key={item.id} className="mb-2">
+              {item.title}
+            </p>
+          ))}
+        </div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Home;
