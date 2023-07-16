d5f1040d54977530bac419db3f6d1ff85cf77156
ww_side <daniil.metelia@gmail.com>
2023-07-16 21:25:29 +0300

    added animation to mainImg

--- src/components/pages/Home/index.tsx
+++ src/components/pages/Home/index.tsx
@@ -1,6 +1,7 @@
 import { FC } from 'react';
 import Footer from '../../common/Footer';
 import { data } from './data.ts';
+import 'animate.css';
 import mainImg from '../../../assets/img/main.png';
 
 const Home: FC = () => {
@@ -8,7 +9,7 @@ const Home: FC = () => {
     <div className="bg-gray-100">
       <div className="flex items-center justify-center gap-7 mt-7 mb-10 max-sm:flex-col">
         <img
-          className="h-[500px] w-[400px] max-md:h-[400px] max-md:w-[300px]"
+          className="h-[500px] w-[400px] max-md:h-[400px] max-md:w-[300px] animate__animated animate__zoomIn"
           src={mainImg}
           alt="iphone"
         />
