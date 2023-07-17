b59a8865fbc6c59ab4b415d454bbae41eea9201d
ww_side <daniil.metelia@gmail.com>
2023-07-17 11:42:15 +0300

    fixed screen height bug

--- src/components/common/Screen/index.tsx
+++ src/components/common/Screen/index.tsx
@@ -1,10 +1,14 @@
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from '../Header/index.tsx';
 
 const Screen: FC = () => {
+  useEffect(() => {
+    document.body.classList.add('bg-gray-100');
+  }, []);
+
   return (
-    <div className="flex flex-col h-screen bg-gray-100">
+    <div className="flex flex-col h-min bg-gray-100">
       <Header />
       <Outlet />
     </div>
